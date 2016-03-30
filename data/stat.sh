f4c6820e8d4c93cdc166931fad3d06daed8df774
Chun-Yuan Huang <huangc@users.noreply.github.com>
2016-03-30 01:02:16 -0400

    Update stat.sh

--- data/stat.sh
+++ data/stat.sh
@@ -11,41 +11,64 @@ echo "## Aim: This is to investigate if NIPPONBARE is a closer relative to sampl
 #----------------------------
 " > stat.result
 
+# Redefine the minimal size of deletion gap for the purpose of InDel fingerprinting
+# which uses a longer gap size for robust comparison
+GAP_MINSIZE_FP=10
+GAP_MAXSIZE_FP=5000
+
+# Refilter the deletion table according to the redefined GAP_MINSIZE.
+# DELSUM=SeqDel_sum_Lg5
+# Tname tGapStart tGapSize Samples
+# OsjChr10 10020508 30 YB4;
+# OsjChr10 10020526 6 YB2;YB3;YB6;YB7;
+
+awk -v gap_minsize="${GAP_MINSIZE_FP}" -v gap_maxsize="${GAP_MAXSIZE_FP}" '{ OFS="\t"; if ( $3 >= gap_minsize && $3 <= gap_maxsize ) print $0 }' \
+ SeqDel_sum_Lg${GAP_MINSIZE}.table > SeqDel_sum_Lg${GAP_MINSIZE_FP}.table
+
+# Setup for comparison
+QUERY=NIPPONBARE
+QUERYNAME=CX140
 SAMPLE="YB1 YB2 YB3 YB4 YB5 YB6 YB7 YB8"
+SAMPLENAME=YB
+
+\rm -f FP*
+
 for i in ${SAMPLE}
 do
-egrep "(NIPPONBARE|$i)" SeqDel_sum_Lg5.table > SeqDel_sum_grep_Nipp_or_${i}.table
-egrep "NIPPONBARE" SeqDel_sum_Lg5.table | egrep "$i" > SeqDel_sum_grep_Nipp_n_${i}.table
+egrep "(${QUERY}|$i)" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table > FP_union_${QUERYNAME}_${i}
+egrep "${QUERY}" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table | egrep "$i" > FP_intcpt_${QUERYNAME}_${i}
 done
 
 
-Tot=`wc -l SeqDel_sum_Lg5.table | cut -d" " -f1`
-echo "Total deletion sites from NIPPONBARE and samples YB1-YB8 are $Tot" >> stat.result
+target=`wc -l SeqDel_sum_Lg${GAP_MINSIZE_FP}.table | cut -d" " -f1`
+echo "Total deletion sites in SeqDel_sum_Lg${GAP_MINSIZE_FP}.table are $target" >> stat.result
 
-Nipp=`grep "NIPPONBARE" SeqDel_sum_Lg5.table | wc -l | cut -d" " -f1`
-echo "Total deletion sites from NIPPONBARE are $Nipp" >> stat.result
+query=`grep "NIPPONBARE" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table | wc -l | cut -d" " -f1`
+echo "Total deletion sites from ${QUERYNAME} are $query" >> stat.result
 
-egrep "(YB1|YB2|YB3|YB4|YB5|YB6|YB7|YB8)" SeqDel_sum_Lg5.table > SeqDel_sum_grep_samples.table
-Samples=`wc -l SeqDel_sum_grep_samples.table  | cut -d" " -f1`
-echo "Total deletion sites from samples YB1-YB8 are ${Samples}" >> stat.result
+# grep union of all samples
+egrep "(YB1|YB2|YB3|YB4|YB5|YB6|YB7|YB8)" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table > FP_union_${SAMPLENAME}
+samples=`wc -l FP_union_${SAMPLENAME} | cut -d" " -f1`
+echo "Total deletion sites from ${SAMPLENAME} samples are ${samples}" >> stat.result
 
-egrep "NIPPONBARE" SeqDel_sum_Lg5.table | egrep "YB1" | egrep "YB2" | \
+# grep intercept of all samples
+egrep "${QUERY}" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table | egrep "YB1" | egrep "YB2" | \
 egrep "YB3" | egrep "YB4" | egrep "YB5" | egrep "YB6" | \
-egrep "YB7" | egrep "YB8" > SeqDel_sum_grep_Nipp_n_all.table
-Nipp_All=`wc -l SeqDel_sum_grep_Nipp_n_all.table  | cut -d" " -f1`
-echo "Total deletion sites that are shared among NIPPONBARE and samples YB1-YB8 are ${Nipp_All}" >> stat.result
+egrep "YB7" | egrep "YB8" > FP_intcpt_${QUERYNAME}_${SAMPLENAME}
+query_samples=`wc -l FP_intcpt_${QUERYNAME}_${SAMPLENAME} | cut -d" " -f1`
+echo "Total deletion sites that are shared among ${QUERYNAME} and ${SAMPLENAME} samples are ${query_samples}" >> stat.result
 echo "" >> stat.result
 
 
 for i in ${SAMPLE}
 do
-Sample=`grep "$i" SeqDel_sum_Lg5.table | wc -l | cut -d" " -f1`
-echo "" >> stat.result
-echo "Total deletion sites from sample ${i} are $Sample" >> stat.result
-Nipp_Sample=`wc -l SeqDel_sum_grep_Nipp_n_${i}.table  | cut -d" " -f1`
-echo "Total deletion sites from NIPPONBARE that are also shared with sample ${i} are ${Nipp_Sample}" >> stat.result
-Perc_sample=`awk "BEGIN { print ${Nipp_Sample}/${Sample}*100 }"`
-echo "Percentage of Nipponbare deletion sites that are shared with ${i} as compared to deletions of sample ${i} is ${Perc_sample}%" >> stat.result
+echo "##" >> stat.result
+sample=`grep "$i" SeqDel_sum_Lg${GAP_MINSIZE_FP}.table | wc -l | cut -d" " -f1`
+echo "Total deletion sites from sample ${i} are $sample" >> stat.result
+query_sample=`wc -l FP_intcpt_${QUERYNAME}_${i}  | cut -d" " -f1`
+echo "Total deletion sites from sample ${i} that are also shared with ${QUERYNAME} are ${query_sample}" >> stat.result
+perc_sample=`awk "BEGIN { print ${query_sample}/${sample}*100 }"`
+echo "Percentage of deletion sites that are shared by ${i} and ${QUERYNAME} as compared to deletion sites of sample ${i} is ${perc_sample}%" >> stat.result
 done
 
 echo "
@@ -59,7 +82,7 @@ echo "
 #4. YB1 has higher percentage (10%) of Nipponbare-shared deletion sites than other YB samples (~3.8%).
 #5. It is likely that YB1 is a cultivar closer to NIPPONBARE than YB2-YB8 are to NIPPONBARE, based on the observations #3 and #4.
 #6. The Indel fingerprinting result is consistent with the SNP fingerprinting result, which shows strong evidence that 
-#   NIPPONBARE is the closest relative among all 3kRGP to YB1, while B182 is the closest relative of all 3kRGP to YB2-YB8. 
+#   NIPPONBARE is the closest relative among all 3kRGP to YB1. 
 
 " >> stat.result
 
